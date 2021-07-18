44bef85500cbc5338c127b7c83565cf13e04e7d6
Caike Laurentino <caike_tb@ymail.com>
2021-07-17 21:48:56 -0300

    1044

---
+++ 1044.js
@@ -0,0 +1,14 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+// URI 1044 | MÚLTIPLOS
+
+var splitted = input.split(" ");
+
+var A = parseInt(splitted.shift());
+var B = parseInt(splitted.shift());
+
+if (A % B == 0 || B % A == 0) {
+	console.log("Sao Multiplos");
+} else {
+	console.log("Nao sao Multiplos");
+}
