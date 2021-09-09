bf41a8c495a6ef0a3ea9a791e5e8fbf115633394
Caike <caike_tb@ymail.com>
2021-09-09 00:41:39 -0300

    Added challenges 1010 to 1015

---
+++ 1014.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+// URI 1014 | CONSUMO
+
+var value = input.split('\n')
+var X = parseInt(value.shift())
+var Y = parseFloat(value.shift())
+
+var consumoMedio = (X / Y).toFixed(3)
+
+console.log(consumoMedio + ' km/l')
