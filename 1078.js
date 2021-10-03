45fad0e88f5b3bc3dcc7355c12e18986225ff3ee
Caike <caike_tb@ymail.com>
2021-10-03 14:38:55 -0300

    Added challenge 1078

---
+++ 1078.js
@@ -0,0 +1,10 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+// 1078 | TABELA DE MULTIPLICAÇÃO
+
+var n = input;
+n = parseInt(n)
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${n} = ${i * n}`)   
+}
