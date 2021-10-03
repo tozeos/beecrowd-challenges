eab475e39c7e142dc097a05fb9912f91470aee3e
Caike <caike_tb@ymail.com>
2021-10-03 14:26:54 -0300

    Added challenge 1060

---
+++ 1060.js
@@ -0,0 +1,13 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split('\n');
+
+// URI 1060 | NÚMEROS POSITIVOS -- ALGO ERRADO
+
+let p = 0;
+
+for (let i = 0; i < 6; i++) {
+    let n = parseFloat(lines.shift().trim())
+    if(n > 0) ++p
+}
+
+console.log(`${p} valores positivos`)
