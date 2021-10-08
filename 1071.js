cfc223b4e830f9e54b957c2edc4933a4d32e78d8
Caike <caike_tb@ymail.com>
2021-10-08 02:32:30 -0300

    Challenge 1071 added

---
+++ 1071.js
@@ -0,0 +1,28 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+let lines = input.split("\r");
+
+// URI 1071 | SOMA DE IMPARES CONSECUTIVOS I
+
+var x = parseInt(lines.shift());
+var y = parseInt(lines.shift());
+var num = 0;
+var sum = 0;
+var i = 0;
+
+if (x < y) {
+	num = x + 1;
+	i = y;
+} else if (x > y) {
+	num = y + 1;
+	i = x;
+}
+
+while (num < i) {
+	if (num % 2 != 0) {
+		sum += num;
+	}
+
+	num++;
+}
+
+console.log(sum);
