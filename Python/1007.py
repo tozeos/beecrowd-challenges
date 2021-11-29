da07ac5e7f29868dcaff02255b74cbd9f02e232f
Caike <caike_tb@ymail.com>
2021-11-28 23:23:16 -0300

    Pastas organizadas por linguagens

---
+++ Python/1007.py
@@ -0,0 +1,10 @@
+# URI 1007 | DIFERENÇA
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+difference = (A * B - C * D)
+
+print("DIFERENCA =", difference)
---
+++ Python/1080.py
@@ -0,0 +1,12 @@
+# BEECROWD 1080 | MAIOR E POSIÇÃO
+
+posicao = 0
+maior = 0
+
+for i in range(1, 101):
+    N = int(input())
+    if(N >= maior):
+        maior = N
+        posicao = i
+print(maior)
+print(posicao)
