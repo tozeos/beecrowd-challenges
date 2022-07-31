4476180d962d29c309724c8f3c58084feb22ccf1
Caike Laurentino <tozeos@outlook.com>
2022-07-31 18:18:44 -0300

    Added challenge 1131 in PHP

---
+++ PHP/1131.php
@@ -0,0 +1,47 @@
+<?php
+
+# BEE 1131 | GRENAIS
+
+$goals = explode(" ", fgets(STDIN));
+$int = (int) $goals[0];
+$gre = (int) $goals[1];
+$newGrenal = 1;
+$intWin = 0;
+$greWin = 0;
+$draw = 0;
+
+while ($newGrenal == 1) {
+    if ($int > $gre) {
+        $intWin+=1;
+    } elseif ($int < $gre) {
+        $greWin+=1;
+    } else {
+        $draw+=1;
+    }
+
+    echo "Novo grenal (1-sim 2-nao)" . PHP_EOL;
+    $newGrenal = fgets(STDIN);
+    if($newGrenal == 1) { 
+    $goals = explode(" ", fgets(STDIN));
+    $int = (int) $goals[0];
+    $gre = (int) $goals[1];
+    }
+}
+
+if($newGrenal == 2) {
+    $totalGrenais = $intWin + $greWin + $draw;
+    echo $totalGrenais . " grenais" . PHP_EOL;
+    echo "Inter:" . $intWin . PHP_EOL;
+    echo "Gremio:" . $greWin . PHP_EOL;
+    echo "Empates:" . $draw . PHP_EOL;
+
+    if($intWin > $greWin && $intWin > $draw) {
+        echo "Inter venceu mais" . PHP_EOL;
+    } elseif($greWin > $intWin && $greWin > $draw) {
+        echo "Gremio venceu mais" . PHP_EOL;
+    } else {
+        echo "Nao houve vencedor" . PHP_EOL;
+    }
+}
+
+
