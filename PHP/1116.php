2fadfc544cc5fe7d8ca1e8ff39eaf4029d2e7829
Caike Laurentino <tozeos@outlook.com>
2022-10-27 00:17:58 -0300

    Added challenge 1116 in PHP

---
+++ PHP/1116.php
@@ -0,0 +1,13 @@
+<?php
+
+$N = (int) fgets(STDIN);
+
+while($N--) {
+    $nums = explode(" ", fgets(STDIN));
+    $X = $nums[0];
+    $Y = $nums[1];
+
+    if ($Y == 0) echo "divisao impossivel" . PHP_EOL;
+    else echo number_format($X/$Y, 1, '.', '') . PHP_EOL;
+
+}
